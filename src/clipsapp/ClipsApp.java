ca8ef52af4c8efa2451cc33fb66136eb2e8730ba
Vigotes <degolego2000@gmail.com>
2020-12-27 16:25:36 -0500

    primer apruebaaa

---
+++ src/clipsapp/ClipsApp.java
@@ -0,0 +1,16 @@
+package clipsapp;
+
+import java.io.File;
+import org.jpl7.*;
+
+public class ClipsApp {
+
+    public static void main(String[] args) {
+    
+        Form x = new Form();
+        x.setVisible(true);
+
+
+    }
+    
+}
